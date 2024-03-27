11369adb4b25cab14256dc44defbd3f8e29bee68
ICodder <charliefournier01@hotmail.com>
2024-03-27 17:15:30 -0400

    mise a jour de la page principal ainsi que son css

--- ProjetTuteurEtudiant/resources/views/welcome.blade.php
+++ ProjetTuteurEtudiant/resources/views/welcome.blade.php
@@ -5,9 +5,68 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Page Principal</title>
+    <link rel="stylesheet" href="principal.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
-    <div class="container-fluid"></div>
-    <div class="col-2"></div>
+
+    <div class="container-fluid h-100">
+        <div class="row text-center h-100">
+            <div class="col-2 h-100 text-start card card-conteneur p-0">
+
+                <div class="col-12 card card-event-design text-center">
+                    <h1 id="text-event "> Catégories </h1>
+                </div>
+            </div>
+                    
+            <div class="col-8">
+
+                <div class="row h-25">
+                    <div class="col-12 card card-event-design text-center">
+                        <h1 id="text-event "> Demande d'aide </h1>
+                    </div>
+                </div>
+
+                <div class="row h-25">
+                    <div class="col-12 card card-event-design text-center">
+                        <h1 id="text-event "> Demande d'aide </h1>
+                    </div>
+                </div>
+
+                <div class="row h-25">
+                    <div class="col-12 card card-event-design text-center">
+                        <h1 id="text-event "> Demande d'aide </h1>
+                    </div>
+                </div>
+
+                <div class="row h-25">
+                    <div class="col-12 card card-event-design text-center">
+                        <h1 id="text-event "> Demande d'aide </h1>
+                    </div>
+                </div>
+
+            </div>
+            <div class="col-2">
+                <div class="row h-50">
+                    <div class="col-12 text-start card card-conteneur p-0">
+
+                        <div class="col-12 card card-event-design text-center">
+                            <h1 id="text-event "> Tuteurs </h1>
+                        </div>
+                    </div>
+                </div>
+                <div class="row h-50">
+                    <div class="col-12 text-start card card-conteneur p-0">
+
+                        <div class="col-12 card card-event-design text-center">
+                            <h1 id="text-event "> Étudiants </h1>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    
 </body>
 </html>
