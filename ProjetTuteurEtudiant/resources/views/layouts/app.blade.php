875fa4c7200ce01355f0bebf7325f1a03f679ede
ICodder <charliefournier01@hotmail.com>
2024-04-07 23:24:09 -0400

    mise a jour de la navbar

--- ProjetTuteurEtudiant/resources/views/layouts/app.blade.php
+++ ProjetTuteurEtudiant/resources/views/layouts/app.blade.php
@@ -17,7 +17,7 @@
     <nav class="navbar navbar-expand-lg navbar-light cegep1 text-center">
         <div class="container-fluid">
 
-            <a class="navbar-brand" href="#">Projet Tutorat</a>
+            <a class="navbar-brand" href="{{route('admin.login')}}">Projet Tutorat</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -28,7 +28,7 @@
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
                     <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Accueil</a>
+                        <a class="nav-link active" aria-current="page" href="{{route('Requetes.index')}}">Accueil</a>
                     </li>
 
                     <li class="nav-item">
