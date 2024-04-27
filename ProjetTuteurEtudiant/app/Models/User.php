72caa4e6f3a81e8d75a6dc0ba3db8340fdde1183
ICodder <charliefournier01@hotmail.com>
2024-04-26 20:31:13 -0400

    test dans user.php en auth

--- ProjetTuteurEtudiant/app/Models/User.php
+++ ProjetTuteurEtudiant/app/Models/User.php
@@ -3,9 +3,11 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
 
-class User extends Model
+class User extends Authenticatable
 {
     protected $fillable = ['matricule','prenom', 'nom', 'email', 'pwd', 'nb_heures', 'role', 'niveau'];
     protected $table = 'usagers';
